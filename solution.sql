d0d9d95401139eae84261142c1fbb8f837c0c883
gfkpth <groeg3@web.de>
2025-05-20 15:07:05 +0200

    fixes to final two tasks

--- solution.sql
+++ solution.sql
@@ -33,25 +33,38 @@ ORDER BY a.au_lname
 
 -- 3. Who are the top 3 authors who have sold the highest number of titles? 
 -- au_id, au_lname, au_fname, number of titles
-SELECT a.au_id, a.au_lname, a.au_fname, COUNT(t.title_id) AS publication_count
+SELECT a.au_id, a.au_lname, a.au_fname, SUM(s.qty) AS sales_count
 FROM authors a 
 JOIN titleauthor ta ON a.au_id = ta.au_id
 JOIN titles t ON ta.title_id = t.title_id
+JOIN sales s ON t.title_id = s.title_id
 GROUP BY a.au_id
-ORDER BY publication_count DESC
+ORDER BY sales_count DESC
 LIMIT 3;
-
--- note that the top 3 is somewhat random here because there are more than 3 authors with 2 publications (currently they seem to be sorted by reverse author_id, the three other authors with 2 publications get dropped)
 
 -- 4. Now modify your solution in Challenge 3 so that the output will display all 23 authors instead of the top 3. 
 -- Note that the authors who have sold 0 titles should also appear in your output (ideally display 0 instead of NULL as the TOTAL). 
 -- Also order your results based on TOTAL from high to low.
 
 -- need to use LEFT JOIN to include authors without title entries
-SELECT a.au_id, a.au_lname, a.au_fname, COUNT(t.title_id) AS TOTAL
+SELECT a.au_id, a.au_lname, a.au_fname,
+	CASE 
+		WHEN SUM(s.qty) IS NULL THEN 0
+		ELSE SUM(s.qty)
+	END AS TOTAL
 FROM authors a 
 LEFT JOIN titleauthor ta ON a.au_id = ta.au_id
 LEFT JOIN titles t ON ta.title_id = t.title_id
+LEFT JOIN sales s ON t.title_id = s.title_id
+GROUP BY a.au_id
+ORDER BY TOTAL DESC;
+
+-- alternative with COALESCE
+SELECT a.au_id, a.au_lname, a.au_fname, COALESCE(SUM(s.qty),0) TOTAL
+FROM authors a 
+LEFT JOIN titleauthor ta ON a.au_id = ta.au_id
+LEFT JOIN titles t ON ta.title_id = t.title_id
+LEFT JOIN sales s ON t.title_id = s.title_id
 GROUP BY a.au_id
 ORDER BY TOTAL DESC;
 
